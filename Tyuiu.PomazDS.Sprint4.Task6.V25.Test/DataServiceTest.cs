ee79b610840fcc65a0b350eadacdc80c954f13d6
admin <zed-n.v@mail.ru>
2023-11-12 19:31:55 +0500

    task6

---
+++ Tyuiu.PomazDS.Sprint4.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PomazDS.Sprint4.Task6.V25.Lib;
+
+namespace Tyuiu.PomazDS.Sprint4.Task6.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string[] array = { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+
+            int result = ds.Calculate(array);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ Tyuiu.PomazDS.Sprint4.Task6.V25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PomazDS.Sprint4.Task6.V25.Lib;
+using autofill_pattern_lib;
+
+namespace Tyuiu.PomazDS.Sprint4.Task6.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            AutoFill ptrn = new AutoFill();
+            Random rnd = new Random();
+
+            ptrn.MainPattern(4, "Двумерные массивы. (генератор случайных чисел)", 6, 25, "Дан строковый массив данных [\"Теннис\", \"Футбол\", \"Крикет\", \"Баскетбол\", \"Бейсбол\", \"Регби\", \"Хоккей\"] используя класс Array подсчитайте количество элементов, длина которых больше 6.");
+
+            string[] array = { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+
+            ptrn.ResultPattern();
+
+            Console.WriteLine(ds.Calculate(array));
+
+            Console.ReadKey();
+        }
+    }
+}
